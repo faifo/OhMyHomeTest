39b285df3de32ee5f692ca1ffbf97a471d3d2ef4
LongVo <long.vu@aeris.net>
2022-04-01 20:34:13 +0700

    feat:create appcoordinator

---
+++ OhMyHome/Application/AppCoordinator.swift
@@ -0,0 +1,24 @@
+//
+//  AppCoordinator.swift
+//  OhMyHome
+//
+//  Created by Solo on 01/04/2022.
+//
+
+import UIKit
+final class AppCoordinator {
+    
+    private let window: UIWindow
+    private let navigationController: UINavigationController!
+    
+    init(window: UIWindow) {
+        self.window = window
+        let searchViewController = SearchViewController()
+        navigationController = UINavigationController(rootViewController: searchViewController)
+    }
+    
+    func start() {
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+}
---
+++ OhMyHome/Application/Search/SearchViewController.swift
@@ -0,0 +1,126 @@
+//
+//  SearchViewController.swift
+//  OhMyHome
+//
+//  Created by Solo on 01/04/2022.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+        return tableView
+    }()
+    
+    lazy var searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.delegate = self
+        return searchBar
+    }()
+    
+    var tableViewBottomConstraint: NSLayoutConstraint?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setupViews()
+    }
+    
+    func setupViews() {
+        view.backgroundColor = .white
+        [searchBar, tableView].forEach {
+            view.addSubview($0)
+        }
+        
+        searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        searchBar.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        searchBar.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        searchBar.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        tableView.topAnchor.constraint(equalTo: searchBar.bottomAnchor).isActive = true
+        tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: view.safeAreaInsets.bottom, right: 0)
+        tableViewBottomConstraint = tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        tableViewBottomConstraint?.isActive = true
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardShow(notification:)), name: UIWindow.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardHide(notification:)), name: UIWindow.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc private func keyboardShow(notification: NSNotification) {
+        guard let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue else {
+            return
+        }
+        tableViewBottomConstraint?.isActive = false
+        tableViewBottomConstraint = tableView.bottomAnchor.constraint(
+            equalTo: view.bottomAnchor,
+            constant: -keyboardSize.size.height)
+        tableViewBottomConstraint?.isActive = true
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+              let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {
+            return
+        }
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            options: UIView.AnimationOptions(rawValue: curve),
+            animations: { [weak self] in
+                self?.view.layoutIfNeeded()
+            }, completion: nil)
+    }
+    
+    @objc private func keyboardHide(notification: NSNotification) {
+        tableViewBottomConstraint?.isActive = false
+        tableViewBottomConstraint = tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        tableViewBottomConstraint?.isActive = true
+        guard let duration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? Double,
+              let curve = notification.userInfo?[UIResponder.keyboardAnimationCurveUserInfoKey] as? UInt else {
+            return
+        }
+        UIView.animate(
+            withDuration: duration,
+            delay: 0,
+            options: UIView.AnimationOptions(rawValue: curve),
+            animations: { [weak self] in
+                self?.view.layoutIfNeeded()
+            }, completion: nil)
+    }
+
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       return UITableViewCell()
+    }
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 56
+    }
+}
+
+extension SearchViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+    }
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+    }
+}
+
